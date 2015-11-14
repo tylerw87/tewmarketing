fac9528d382e78452047295a4fb6796e575cddad
Tyler Willis <tyler.tewmarketing@gmail.com>
2015-11-13 22:26:09 -0500

    M

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get 'pages/portfolio'
 
   root 'pages#home'
+  
   resources :contacts
 
   # The priority is based upon order of creation: first created -> highest priority.
