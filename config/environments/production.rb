bbef640a3bc97ee2faf69efb6365eb07593fa5b8
Tyler Willis <tyler.tewmarketing@gmail.com>
2015-11-19 13:50:13 -0500

    add staging to gemfile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,6 +23,7 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  # config.assets.precompile += %w(custom.css.scss application.css.scss style1.css.scss styleIE.css.scss)
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
