896575d0ad0c4f74738428e274a199c393b5ad09
Tyler Willis <tyler.tewmarketing@gmail.com>
2015-11-19 14:51:49 -0500

    updated application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,4 +17,14 @@ def markdown(text)
   markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
   markdown_to_html.render(text).html_safe
 end
+ before_action :redirect_to_www if Rails.env == "production"
+
+ def redirect_to_www
+   unless /www\.tewmarketing\.com/ =~ request.url
+     uri = URI.parse(request.url)
+     uri.host = "www.tewmarketing.com"
+     redirect_to uri.to_s
+   end
+ end
+
 end
