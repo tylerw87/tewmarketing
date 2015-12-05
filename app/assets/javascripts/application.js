62260395f08899b44a3847cf0eb3d3219e33d381
Tyler Willis <tyler.tewmarketing@gmail.com>
2015-12-05 00:44:29 -0500

    homepage overhaul

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,11 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require_tree .
+
+function toggle_visibility(id) {
+   var e = document.getElementById(id);
+   if(e.style.display == 'block')
+      e.style.display = 'none';
+   else
+      e.style.display = 'block';
+}
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,11 +2,34 @@ class PagesController < ApplicationController
 	def about
 	end
 	def home
+  	@contact = Contact.new		
 	end
 	def portfolio
 	end
 	def services
 	end
 	def test
+	end
+	def new
+  	@contact = Contact.new
+	end
+
+	def create
+	  @contact = Contact.new(contact_params)
+	  if @contact.save
+	    name = params[:contact][:name]
+	    email = params[:contact][:email]
+	    body = params[:contact][:comments]
+	    ContactMailer.contact_email(name, email, body).deliver
+	    flash[:success] = 'Message sent.'
+	    redirect_to new_contact_path
+	  else
+	    flash[:danger] = 'Error occured, message has not been sent.'
+	    redirect_to new_contact_path
+	  end
+	end
+	private
+	def contact_params
+	  params.require(:contact).permit(:name, :email, :comments)
 	end
 end
